0264e333223a612a634d053390df8f31a46c3f19
Bagus dewantoro <bagusdew0708@nusamandiri.ac.id>
2020-04-15 17:50:14 +0700

    make master data peserta

--- database/migrations/2020_04_15_085616_create_master_pesertas_table.php
+++ database/migrations/2020_04_15_085616_create_master_pesertas_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateKaryawansTable extends Migration
+class CreateMasterPesertasTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,9 @@ class CreateKaryawansTable extends Migration
      */
     public function up()
     {
-        Schema::create('karyawans', function (Blueprint $table) {
+        Schema::create('master_pesertas', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('id_card');
             $table->string('name');
             $table->string('email')->unique();
             $table->date('birthdate');
@@ -32,6 +33,6 @@ class CreateKaryawansTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('karyawans');
+        Schema::dropIfExists('master_pesertas');
     }
 }
