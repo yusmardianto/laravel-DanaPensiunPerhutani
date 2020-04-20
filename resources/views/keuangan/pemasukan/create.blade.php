9285f64077166e231777066f3cefba2da3c2c0b6
Bagus Nurcahyo Putra <bagusnurcahyoputra@gmail.com>
2020-04-20 23:55:49 +0700

    before pull

--- resources/views/keuangan/pemasukan/create.blade.php
+++ resources/views/keuangan/pemasukan/create.blade.php
@@ -84,7 +84,7 @@
         <div class="col-lg-12">
             <div class="ibox ">
                 <div class="ibox-title">
-                    <h5>Input Data pemasukan</h5>
+                    <h5>Input Data Pemasukan</h5>
                     <div class="ibox-tools">
                         <a href="{{ url('keuangan/pemasukan') }}" class="btn btn-primary btn-xs modal-form">
                             <i class="fa fa-arrow-circle-o-left"></i>
