f8f850db11ca9ac729b9a96391ee66506c384e3d
Bagus Nurcahyo Putra <bagusnurcahyoputra@gmail.com>
2020-04-21 13:54:20 +0700

    before pull

--- routes/web.php
+++ routes/web.php
@@ -98,48 +98,6 @@ Route::group(['middleware' => ['auth']], function() {
         Route::any('periode/ajax-list', 'PeriodeController@ajaxList');
     });
 
-    Route::prefix('config')->group(function() {
-        //module
-        Route::get('module', 'ModuleController@index');
-        Route::any('module/ajax-list', 'ModuleController@ajaxList');
-        Route::get('module/create', 'ModuleController@getCreate');
-        Route::post('module/create', 'ModuleController@postCreate');
-        Route::get('module/detail/{id}', 'ModuleController@getDetail');
-        Route::any('module/ajax-permission/{id}', 'ModuleController@ajaxPermission');
-        Route::get('module/add-permission/{id}','ModuleController@getAddPermission');
-        Route::post('module/add-permission/{id}', 'ModuleController@postAddPermission');
-        Route::post('module/delete-permission/{id}/{permissionId}', 'ModuleController@postDeletePermission');
-        Route::get('module/edit/{id}', 'ModuleController@getEdit');
-        Route::post('module/edit/{id}', 'ModuleController@postEdit');
-        Route::post('module/delete/{id}', 'ModuleController@delete');
-        //role
-        Route::get('role', 'RoleController@index');
-        Route::any('role/ajax-list', 'RoleController@ajaxList');
-        Route::get('role/create', 'RoleController@getCreate');
-        Route::post('role/create', 'RoleController@postCreate');
-        Route::get('role/detail/{id}', 'RoleController@getDetail');
-        Route::any('role/ajax-user/{id}', 'RoleController@ajaxUser');
-        Route::get('role/add-user/{id}', 'RoleController@getAddUser');
-        Route::post('role/add-user/{id}', 'RoleController@postAddUser');
-        Route::post('role/delete-user/{id}/{userId}', 'RoleController@postDeleteUser');
-        Route::get('role/edit/{id}', 'RoleController@getEdit');
-        Route::post('role/edit/{id}', 'RoleController@postEdit');
-        Route::post('role/delete/{id}', 'RoleController@delete');
-        //user
-        Route::get('user', 'UserController@index');
-        Route::any('user/ajax-list', 'UserController@ajaxList');
-        Route::get('user/create', 'UserController@getCreate');
-        Route::post('user/create', 'UserController@postCreate');
-        Route::get('user/detail/{id}', 'UserController@getDetail');
-        Route::any('user/ajax-role/{id}', 'UserController@ajaxRole');
-        Route::get('user/add-role/{id}', 'UserController@getAddRole');
-        Route::post('user/add-role/{id}', 'UserController@postAddRole');
-        Route::post('user/delete-role/{id}/{roleId}', 'UserController@postDeleteRole');
-        Route::get('user/edit/{id}', 'UserController@getEdit');
-        Route::post('user/edit/{id}', 'UserController@postEdit');
-        Route::post('user/delete/{id}', 'UserController@delete');
-    });
-
     Route::prefix('kepesertaan')->group(function() {
         Route::get('peserta-aktif', 'PesertaAktifController@index');
         Route::any('peserta-aktif/ajax-list', 'PesertaAktifController@ajaxList');
@@ -181,5 +139,47 @@ Route::group(['middleware' => ['auth']], function() {
 
         //transaksi pengeluaran kas
         Route::get('pengeluaran', 'PengeluaranController@index');
+    });
+
+    Route::prefix('config')->group(function() {
+        //module
+        Route::get('module', 'ModuleController@index');
+        Route::any('module/ajax-list', 'ModuleController@ajaxList');
+        Route::get('module/create', 'ModuleController@getCreate');
+        Route::post('module/create', 'ModuleController@postCreate');
+        Route::get('module/detail/{id}', 'ModuleController@getDetail');
+        Route::any('module/ajax-permission/{id}', 'ModuleController@ajaxPermission');
+        Route::get('module/add-permission/{id}','ModuleController@getAddPermission');
+        Route::post('module/add-permission/{id}', 'ModuleController@postAddPermission');
+        Route::post('module/delete-permission/{id}/{permissionId}', 'ModuleController@postDeletePermission');
+        Route::get('module/edit/{id}', 'ModuleController@getEdit');
+        Route::post('module/edit/{id}', 'ModuleController@postEdit');
+        Route::post('module/delete/{id}', 'ModuleController@delete');
+        //role
+        Route::get('role', 'RoleController@index');
+        Route::any('role/ajax-list', 'RoleController@ajaxList');
+        Route::get('role/create', 'RoleController@getCreate');
+        Route::post('role/create', 'RoleController@postCreate');
+        Route::get('role/detail/{id}', 'RoleController@getDetail');
+        Route::any('role/ajax-user/{id}', 'RoleController@ajaxUser');
+        Route::get('role/add-user/{id}', 'RoleController@getAddUser');
+        Route::post('role/add-user/{id}', 'RoleController@postAddUser');
+        Route::post('role/delete-user/{id}/{userId}', 'RoleController@postDeleteUser');
+        Route::get('role/edit/{id}', 'RoleController@getEdit');
+        Route::post('role/edit/{id}', 'RoleController@postEdit');
+        Route::post('role/delete/{id}', 'RoleController@delete');
+        //user
+        Route::get('user', 'UserController@index');
+        Route::any('user/ajax-list', 'UserController@ajaxList');
+        Route::get('user/create', 'UserController@getCreate');
+        Route::post('user/create', 'UserController@postCreate');
+        Route::get('user/detail/{id}', 'UserController@getDetail');
+        Route::any('user/ajax-role/{id}', 'UserController@ajaxRole');
+        Route::get('user/add-role/{id}', 'UserController@getAddRole');
+        Route::post('user/add-role/{id}', 'UserController@postAddRole');
+        Route::post('user/delete-role/{id}/{roleId}', 'UserController@postDeleteRole');
+        Route::get('user/edit/{id}', 'UserController@getEdit');
+        Route::post('user/edit/{id}', 'UserController@postEdit');
+        Route::post('user/delete/{id}', 'UserController@delete');
     });
 });
