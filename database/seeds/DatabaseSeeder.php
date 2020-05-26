00c91a21103be1a7d36a9c213d145c1604b00708
rochman nursyaid <nursyaidr@gmail.com>
2020-05-27 03:05:13 +0700

    crud dan seed rapel extra manfaat

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -28,5 +28,6 @@ class DatabaseSeeder extends Seeder
         $this->call(RegenciesTableSeeder::class);
         $this->call(ReligionsTableSeeder::class);
         $this->call(GendersTableSeeder::class);
+        $this->call(JenisTransaksiTableSeeder::class);
     }
 }
