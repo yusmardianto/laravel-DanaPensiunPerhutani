d29804fb80a508f26e5fbca288f31316e1834f38
rochman nursyaid <nursyaidr@gmail.com>
2020-05-27 03:11:39 +0700

    crud seed rapel extra manfaat

--- database/seeds/JenisTransaksiTableSeeder.php
+++ database/seeds/JenisTransaksiTableSeeder.php
@@ -40,52 +40,45 @@ class JenisTransaksiTableSeeder extends Seeder
             array(
                 'id' => 4,
                 'name' => 'Extra Manfaat Pensiun Bulanan',
-                'created_at' => '2020-05-22 14:09:03',
+                'created_at' => NULL,
                 'updated_at' => NULL,
             ),
             4 =>
             array(
                 'id' => 5,
-                'name' => 'Budha',
+                'name' => 'MP Bulanan - Potongan Persatuan Pensiunan',
                 'created_at' => NULL,
                 'updated_at' => NULL,
             ),
             5 =>
             array(
                 'id' => 6,
-                'name' => 'MP Bulanan - Potongan Persatuan Pensiunan',
+                'name' => 'MP Bulanan - Potongan Koperasi',
                 'created_at' => NULL,
                 'updated_at' => NULL,
             ),
             6 =>
             array(
                 'id' => 7,
-                'name' => 'MP Bulanan - Potongan Koperasi',
-                'created_at' => NULL,
-                'updated_at' => NULL,
-            ),
-            7 =>
-            array(
-                'id' => 7,
                 'name' => 'MP Bulanan - Potongan Pinjaman',
                 'created_at' => NULL,
                 'updated_at' => NULL,
             ),
-            8 =>
+            7 =>
             array(
                 'id' => 8,
                 'name' => 'Extra MP Bulanan - Lain',
                 'created_at' => NULL,
                 'updated_at' => NULL,
             ),
-            9 =>
+            8 =>
             array(
                 'id' => 9,
                 'name' => 'MP Bulanan - Potongan Lain 1',
                 'created_at' => NULL,
                 'updated_at' => NULL,
             ),
-            10 =>
+            9 =>
             array(
                 'id' => 10,
                 'name' => 'MP Bulanan - Potongan Lain 2',
