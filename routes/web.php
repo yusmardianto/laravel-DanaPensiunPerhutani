6d999b315473441f4b8252d3a851667df02c4bd1
Bagus Nurcahyo Putra <bagusnurcahyoputra@gmail.com>
2020-05-27 12:35:38 +0700

    rapihin route web

--- routes/web.php
+++ routes/web.php
@@ -190,80 +190,81 @@ Route::group(['middleware' => ['auth']], function() {
             Route::post('rapelextramanfaat/destroy/{id}', 'RapelExtraManfaatControllerr@destroy');
         });
 
-        Route::prefix('investasi')->group(function () {
-            //data transaksi
-            Route::get('datatransaksi', 'DataTransaksiController@index');
-            //status order
-            Route::get('statusorder', 'StatusOrderController@index');
-            //perubahan status order
-            Route::get('perubahanstatusorder', 'PerubahanStatusOrderController@index');
-            //surat instruksi
-            Route::get('suratinstruksi', 'SuratInstruksiController@index');
-        });
+    });
 
-        Route::prefix('keuangan')->group(function () {
-            //transaksi pemasukan kas
-            Route::get('pemasukan', 'PemasukanController@index');
-            Route::any('pemasukan/ajax-list', 'PemasukanController@ajaxList');
-            Route::get('pemasukan/create', 'PemasukanController@getCreate');
-            Route::post('pemasukan/create', 'PemasukanController@postCreate');
-            Route::get('pemasukan/detail/{id}', 'PemasukanController@getDetail');
-            Route::get('pemasukan/edit/{id}', 'PemasukanController@getEdit');
-            Route::post('pemasukan/edit/{id}', 'PemasukanController@postEdit');
-            Route::post('pemasukan/destroy/{id}', 'PemasukanController@destroy');
+    Route::prefix('investasi')->group(function () {
+        //data transaksi
+        Route::get('datatransaksi', 'DataTransaksiController@index');
+        //status order
+        Route::get('statusorder', 'StatusOrderController@index');
+        //perubahan status order
+        Route::get('perubahanstatusorder', 'PerubahanStatusOrderController@index');
+        //surat instruksi
+        Route::get('suratinstruksi', 'SuratInstruksiController@index');
+    });
 
-            //transaksi pengeluaran kas
-            Route::get('pengeluaran', 'PengeluaranController@index');
-            Route::any('pengeluaran/ajax-list', 'PengeluaranController@ajaxList');
-            Route::get('pengeluaran/create', 'PengeluaranController@getCreate');
-            Route::post('pengeluaran/create', 'PengeluaranController@postCreate');
-            Route::get('pengeluaran/detail/{id}', 'PengeluaranController@getDetail');
-            Route::get('pengeluaran/edit/{id}', 'PengeluaranController@getEdit');
-            Route::post('pengeluaran/edit/{id}', 'PengeluaranController@postEdit');
-            Route::post('pengeluaran/destroy/{id}', 'PengeluaranController@destroy');
-        });
+    Route::prefix('keuangan')->group(function () {
+        //transaksi pemasukan kas
+        Route::get('pemasukan', 'PemasukanController@index');
+        Route::any('pemasukan/ajax-list', 'PemasukanController@ajaxList');
+        Route::get('pemasukan/create', 'PemasukanController@getCreate');
+        Route::post('pemasukan/create', 'PemasukanController@postCreate');
+        Route::get('pemasukan/detail/{id}', 'PemasukanController@getDetail');
+        Route::get('pemasukan/edit/{id}', 'PemasukanController@getEdit');
+        Route::post('pemasukan/edit/{id}', 'PemasukanController@postEdit');
+        Route::post('pemasukan/destroy/{id}', 'PemasukanController@destroy');
 
-        Route::prefix('config')->group(function () {
-            //module
-            Route::get('module', 'ModuleController@index');
-            Route::any('module/ajax-list', 'ModuleController@ajaxList');
-            Route::get('module/create', 'ModuleController@getCreate');
-            Route::post('module/create', 'ModuleController@postCreate');
-            Route::get('module/detail/{id}', 'ModuleController@getDetail');
-            Route::any('module/ajax-permission/{id}', 'ModuleController@ajaxPermission');
-            Route::get('module/add-permission/{id}', 'ModuleController@getAddPermission');
-            Route::post('module/add-permission/{id}', 'ModuleController@postAddPermission');
-            Route::post('module/delete-permission/{id}/{permissionId}', 'ModuleController@postDeletePermission');
-            Route::get('module/edit/{id}', 'ModuleController@getEdit');
-            Route::post('module/edit/{id}', 'ModuleController@postEdit');
-            Route::post('module/delete/{id}', 'ModuleController@delete');
-            //role
-            Route::get('role', 'RoleController@index');
-            Route::any('role/ajax-list', 'RoleController@ajaxList');
-            Route::get('role/create', 'RoleController@getCreate');
-            Route::post('role/create', 'RoleController@postCreate');
-            Route::get('role/detail/{id}', 'RoleController@getDetail');
-            Route::any('role/ajax-user/{id}', 'RoleController@ajaxUser');
-            Route::get('role/add-user/{id}', 'RoleController@getAddUser');
-            Route::post('role/add-user/{id}', 'RoleController@postAddUser');
-            Route::post('role/delete-user/{id}/{userId}', 'RoleController@postDeleteUser');
-            Route::get('role/edit/{id}', 'RoleController@getEdit');
-            Route::post('role/edit/{id}', 'RoleController@postEdit');
-            Route::post('role/delete/{id}', 'RoleController@delete');
-            //user
-            Route::get('user', 'UserController@index');
-            Route::any('user/ajax-list', 'UserController@ajaxList');
-            Route::get('user/create', 'UserController@getCreate');
-            Route::post('user/create', 'UserController@postCreate');
-            Route::get('user/detail/{id}', 'UserController@getDetail');
-            Route::any('user/ajax-role/{id}', 'UserController@ajaxRole');
-            Route::get('user/add-role/{id}', 'UserController@getAddRole');
-            Route::post('user/add-role/{id}', 'UserController@postAddRole');
-            Route::post('user/delete-role/{id}/{roleId}', 'UserController@postDeleteRole');
-            Route::get('user/edit/{id}', 'UserController@getEdit');
-            Route::post('user/edit/{id}', 'UserController@postEdit');
-           Route::post('user/delete/{id}', 'UserController@delete');
-        });
+        //transaksi pengeluaran kas
+        Route::get('pengeluaran', 'PengeluaranController@index');
+        Route::any('pengeluaran/ajax-list', 'PengeluaranController@ajaxList');
+        Route::get('pengeluaran/create', 'PengeluaranController@getCreate');
+        Route::post('pengeluaran/create', 'PengeluaranController@postCreate');
+        Route::get('pengeluaran/detail/{id}', 'PengeluaranController@getDetail');
+        Route::get('pengeluaran/edit/{id}', 'PengeluaranController@getEdit');
+        Route::post('pengeluaran/edit/{id}', 'PengeluaranController@postEdit');
+        Route::post('pengeluaran/destroy/{id}', 'PengeluaranController@destroy');
+    });
+
+    Route::prefix('config')->group(function () {
+        //module
+        Route::get('module', 'ModuleController@index');
+        Route::any('module/ajax-list', 'ModuleController@ajaxList');
+        Route::get('module/create', 'ModuleController@getCreate');
+        Route::post('module/create', 'ModuleController@postCreate');
+        Route::get('module/detail/{id}', 'ModuleController@getDetail');
+        Route::any('module/ajax-permission/{id}', 'ModuleController@ajaxPermission');
+        Route::get('module/add-permission/{id}', 'ModuleController@getAddPermission');
+        Route::post('module/add-permission/{id}', 'ModuleController@postAddPermission');
+        Route::post('module/delete-permission/{id}/{permissionId}', 'ModuleController@postDeletePermission');
+        Route::get('module/edit/{id}', 'ModuleController@getEdit');
+        Route::post('module/edit/{id}', 'ModuleController@postEdit');
+        Route::post('module/delete/{id}', 'ModuleController@delete');
+        //role
+        Route::get('role', 'RoleController@index');
+        Route::any('role/ajax-list', 'RoleController@ajaxList');
+        Route::get('role/create', 'RoleController@getCreate');
+        Route::post('role/create', 'RoleController@postCreate');
+        Route::get('role/detail/{id}', 'RoleController@getDetail');
+        Route::any('role/ajax-user/{id}', 'RoleController@ajaxUser');
+        Route::get('role/add-user/{id}', 'RoleController@getAddUser');
+        Route::post('role/add-user/{id}', 'RoleController@postAddUser');
+        Route::post('role/delete-user/{id}/{userId}', 'RoleController@postDeleteUser');
+        Route::get('role/edit/{id}', 'RoleController@getEdit');
+        Route::post('role/edit/{id}', 'RoleController@postEdit');
+        Route::post('role/delete/{id}', 'RoleController@delete');
+        //user
+        Route::get('user', 'UserController@index');
+        Route::any('user/ajax-list', 'UserController@ajaxList');
+        Route::get('user/create', 'UserController@getCreate');
+        Route::post('user/create', 'UserController@postCreate');
+        Route::get('user/detail/{id}', 'UserController@getDetail');
+        Route::any('user/ajax-role/{id}', 'UserController@ajaxRole');
+        Route::get('user/add-role/{id}', 'UserController@getAddRole');
+        Route::post('user/add-role/{id}', 'UserController@postAddRole');
+        Route::post('user/delete-role/{id}/{roleId}', 'UserController@postDeleteRole');
+        Route::get('user/edit/{id}', 'UserController@getEdit');
+        Route::post('user/edit/{id}', 'UserController@postEdit');
+        Route::post('user/delete/{id}', 'UserController@delete');
     });
 
 });
